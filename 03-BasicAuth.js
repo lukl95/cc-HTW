fe22dfe055472c2a3981ffbef38c349e7eeedcec
lukl95 <lukasl95@icloud.com>
2017-12-09 11:25:07 +0100

    Update 03-BasicAuth.js

--- 03-BasicAuth.js
+++ 03-BasicAuth.js
@@ -2,6 +2,7 @@ var http = require('http');
 var url = require('url');
 var crypto = require("crypto");
 var port = process.env.PORT || 8081;
+var proc = require('process');
 
 http.createServer(function (request, response) {
    // Send the HTTP header 
@@ -13,6 +14,7 @@ http.createServer(function (request, response) {
     var firstname =  queryData.fn;
     var lastname =  queryData.ln;
     var requestId = crypto.randomBytes(16).toString("hex");
+    var procId = proc.pid;
 
     var headers = request.headers;
     var authorization = headers['authorization'];
@@ -39,7 +41,7 @@ http.createServer(function (request, response) {
         }).on('end', function() {
             body = Buffer.concat(body).toString();
                 // at this point, `body` has the entire request body stored in it as a string
-                var responseMessage = 'Your provided e-mail address is: '+body;
+                var responseMessage = 'Process-ID: '+procId+'\nYour provided e-mail address is: '+body;
                 
                 if (authorization) 
                     responseMessage += '\nYour provided Authorization header is: '+authorization;
